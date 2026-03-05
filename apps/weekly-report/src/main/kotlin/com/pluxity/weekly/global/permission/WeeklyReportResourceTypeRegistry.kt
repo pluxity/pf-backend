9fa2bcc64ec70b94dff9e2fbc0a1f32612c6bd55
inyeop <kim41516@pluxity.com>
2026-03-05 16:00:16 +0900

    feat: 프로젝트 관리 기능에 권한 체크 로직 추가
    - ProjectService 메서드에 @CheckPermission 애너테이션 추가로 CRUD 권한 체크 구현
    - WeeklyReportResourceTypeRegistry에 PROJECT 리소스 타입 등록

--- apps/weekly-report/src/main/kotlin/com/pluxity/weekly/global/permission/WeeklyReportResourceTypeRegistry.kt
+++ apps/weekly-report/src/main/kotlin/com/pluxity/weekly/global/permission/WeeklyReportResourceTypeRegistry.kt
@@ -14,6 +14,7 @@ class WeeklyReportResourceTypeRegistry : ResourceTypeRegistry {
     ) {
         USER("사용자관리", "users"),
         TEAM("팀관리", "teams"),
+        PROJECT("프로젝트관리", "projects"),
     }
 
     override fun resolve(name: String): String =
