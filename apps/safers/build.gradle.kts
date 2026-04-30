73ca65dacb399a23b5b260f5abe26aad43912dda
inyeop16 <kim41516@pluxity.com>
2026-04-30 15:30:17 +0900

    chore(safers): InfluxDB 클라이언트 의존성 + 환경별 설정 추가

--- apps/safers/build.gradle.kts
+++ apps/safers/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(rootProject.libs.bundles.coroutines)
     implementation(rootProject.libs.logbook)
     implementation(rootProject.libs.spring.boot.starter.data.redis)
+    implementation(rootProject.libs.influxdb)
 
     // Hibernate Spatial + JTS (Site polygon)
     implementation(rootProject.libs.bundles.spatial)
